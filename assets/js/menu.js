b2e44c99572efebe038e3bca0c261e11dd20d48c
Dayvison <dayvison.ribeiro28@gmail.com>
2019-12-16 18:34:37 -0300

    enviando os primeiros arquivos do site de anding page

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+/*const $pai_mobile = document.querySelector('.pai_mobile')
+const $menumobile = document.querySelector('.menumobile')
+const $menumobile_open = document.querySelector('.pai_mobile_open')
+$menumobile.addEventListener('click',fuction(){
+  $pai_mobile.classList.add('pai_mobile_open')
+})
+$menumobile_open.addEventListener('click',fuction(){
+  $pai_mobile.classList.remove('pai_mobile_open')
+})
+*/
